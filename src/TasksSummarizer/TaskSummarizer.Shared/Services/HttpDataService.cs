fe00b4c3589b186a2c2ca9aa8cdbfef9221a1e60
Jcardif <josh.cardif14@outlook.com>
2023-03-14 20:38:50 +0300

    complete httpdata service neede methods

--- src/TasksSummarizer/TaskSummarizer.Shared/Services/HttpDataService.cs
+++ src/TasksSummarizer/TaskSummarizer.Shared/Services/HttpDataService.cs
@@ -4,7 +4,171 @@ using System.Text;
 
 namespace TaskSummarizer.Shared.Services
 {
-    internal class HttpDataService
+    public class HttpDataService
     {
+        private readonly HttpClient client;
+        private readonly Dictionary<string, object> responseCache;
+
+        public HttpDataService(string defaultBaseUrl)
+        {
+
+            client = new HttpClient();
+
+            if (!string.IsNullOrEmpty(defaultBaseUrl))
+                client.BaseAddress = defaultBaseUrl.EndsWith("/")
+                    ? new Uri(defaultBaseUrl)
+                    : new Uri($"{defaultBaseUrl}/");
+
+            responseCache = new Dictionary<string, object>();
+        }
+
+        public async Task<T> GetAsync<T>(string uri, string accessToken = null,
+            List<(string name, List<string> value)> headers = null, bool forceRefresh = false)
+        {
+            T result = default;
+
+            // If url ends with a slash remove the slash
+            if (uri.EndsWith("/"))
+                uri = uri.Remove(uri.Length - 1);
+
+            // If header parameters are provided append them to the url
+            if (headers != null)
+            {
+                foreach (var header in headers) client.DefaultRequestHeaders.Add(header.name, header.value);
+
+                // Append the ? to begin the header parameters sequence
+                uri = $"{uri}?";
+
+                // Loop through the parameters and append them to the url
+                for (var i = 0; i < headers.Count; i++)
+                {
+                    if (i < 1)
+                        uri += $"{headers[i].name}={headers[i].value}";
+                    if (i >= 1)
+                        uri += $"&{headers[i].name}={headers[i].value}";
+                }
+            }
+
+            // The responseCache is a simple store of past responses to avoid unnecessary requests for the same resource.
+            // Feel free to remove it or extend this request logic as appropraite for your app.
+            if (forceRefresh || !responseCache.ContainsKey(uri))
+            {
+                if (accessToken != null)
+                    AddAuthorizationHeader(accessToken);
+
+                var response = await client.GetAsync(uri);
+
+                if (!response.IsSuccessStatusCode)
+                    return default;
+
+                var json = await response.Content.ReadAsStringAsync();
+                result = await Task.Run(() => JsonConvert.DeserializeObject<T>(json));
+
+                if (responseCache.ContainsKey(uri))
+                    responseCache[uri] = result;
+                else
+                    responseCache.Add(uri, result);
+            }
+            else
+            {
+                result = (T)responseCache[uri];
+            }
+
+            return result;
+        }
+
+        public async Task<bool> PostAsync<T>(string uri, T item)
+        {
+            if (item == null) return false;
+
+            var serializedItem = JsonConvert.SerializeObject(item);
+            var buffer = Encoding.UTF8.GetBytes(serializedItem);
+            var byteContent = new ByteArrayContent(buffer);
+
+            var response = await client.PostAsync(uri, byteContent);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<object> PostAsJsonAsync<T>(string uri, T item, string accessToken)
+        {
+            if (item == null)
+                return null;
+            AddAuthorizationHeader(accessToken);
+
+            var serializedItem = await Task.Run(() => JsonConvert.SerializeObject(item));
+            var stringContent = new StringContent(serializedItem, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(uri, stringContent);
+
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == HttpStatusCode.Created)
+                return await Json.ToObjectAsync<T>(json);
+
+            return await Json.ToObjectAsync<object>(json);
+
+            //TODO : Handle other status codes
+        }
+
+
+        public async Task<bool> PutAsync<T>(string uri, T item)
+        {
+            if (item == null) return false;
+
+            var serializedItem = JsonConvert.SerializeObject(item);
+            var buffer = Encoding.UTF8.GetBytes(serializedItem);
+            var byteContent = new ByteArrayContent(buffer);
+
+            var response = await client.PutAsync(uri, byteContent);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> PutAsJsonAsync<T>(string uri, T item, string accessToken)
+        {
+            if (item == null) return false;
+
+            AddAuthorizationHeader(accessToken);
+            var serializedItem = JsonConvert.SerializeObject(item);
+
+            var response =
+                await client.PutAsync(uri, new StringContent(serializedItem, Encoding.UTF8, "application/json"));
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> PutAsJsonAsync(string uri, string accessToken)
+        {
+            AddAuthorizationHeader(accessToken);
+
+            var response =
+                await client.PutAsync(uri, null);
+
+            return response.IsSuccessStatusCode;
+        }
+
+
+        public async Task<bool> DeleteAsync(string uri)
+        {
+            var response = await client.DeleteAsync(uri);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        // Add this to all public methods
+        private void AddAuthorizationHeader(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                client.DefaultRequestHeaders.Authorization = null;
+                return;
+            }
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
     }
 }
--- src/TasksSummarizer/TasksSummarizer.Functions/Functions/SummarizeTasksHttpTrigger.cs
+++ src/TasksSummarizer/TasksSummarizer.Functions/Functions/SummarizeTasksHttpTrigger.cs
@@ -34,37 +34,6 @@ namespace TasksSummarizer.Functions.Functions
             var resourceName = config.GetValue<string>("AzureOpenAI:ResourceName");
             var deploymentId = config.GetValue<string>("AzureOpenAI:DeploymentId");
 
-
-            OpenAIAPI api = OpenAIAPI.ForAzure(apiKey, resourceName, deploymentId);
-
-            var chat = api.Chat.CreateConversation();
-
-            /// give instruction as System
-            chat.AppendSystemMessage("You are a teacher who helps children understand if things are animals or not.  If the user tells you an animal, you say \"yes\".  If the user tells you something that is not an animal, you say \"no\".  You only ever respond with \"yes\" or \"no\".  You do not say anything else.");
-
-            // give a few examples as user and assistant
-            chat.AppendUserInput("Is this an animal? Cat");
-            chat.AppendExampleChatbotOutput("Yes");
-            chat.AppendUserInput("Is this an animal? House");
-            chat.AppendExampleChatbotOutput("No");
-
-            // now let's ask it a question'
-            chat.AppendUserInput("Is this an animal? Dog");
-            // and get the response
-            string response = await chat.GetResponseFromChatbot();
-            _logger.LogInformation(response); // "Yes"
-
-            // and continue the conversation by asking another
-            chat.AppendUserInput("Is this an animal? Chair");
-            // and get another response
-            response = await chat.GetResponseFromChatbot();
-            _logger.LogInformation(response); // "No"
-
-            // the entire chat history is available in chat.Messages
-            foreach (ChatMessage msg in chat.Messages)
-            {
-                Console.WriteLine($"{msg.Role}: {msg.Content}");
-            }
             
 
             return req.CreateResponse(HttpStatusCode.OK);
